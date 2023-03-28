1b800c58e6e0a7e6290696b6250a5415bc1d2445
MdotSouza <micheljssouza@gmail.com>
2023-03-28 02:23:38 -0300

    Criação da função soma

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -1,4 +1,13 @@
-from funcoes import soma, subtracao, divisao, multiplicacao
+from projetos.calculadora.funcoes import soma, subtracao, divisao, multiplicacao
 
 def calcule():
-    pass
+    menuFuncoes = {
+        'soma':soma, 'subtracao':subtracao, 'divisao':divisao, 'multiplicacao':multiplicacao, '+': soma, '-':subtracao, '*':multiplicacao, '/':divisao
+    }
+    a, b = input("Digite o número a: "), input("Digite o número b: ")
+    opcao = input(f"Selecione a operaço: {[opcao for opcao in menuFuncoes.keys()]}: ")
+    
+    if opcao in menuFuncoes:
+      print(menuFuncoes[opcao](a,b))
+    else:
+      print("Seleção Inválida")
--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -1,5 +1,9 @@
-def soma():
-    ...
+def soma(a,b):
+    try:
+        a, b = int(a), int(b)
+        return a + b
+    except ValueError as e:
+        print(f"O input 'a' e 'b' devem ser um int ou float.")
 
 def subtracao():
     ...
